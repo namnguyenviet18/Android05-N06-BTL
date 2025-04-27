19352abd81557f544b37ddb4d98c321f15c6ae2a
namnguyenviet18 <nguyenvietnam18012003@gmail.com>
2025-04-27 13:45:10 +0700

    remove tracked files to apply .gitignore

--- music_app_mobile/app/src/main/java/com/group06/music_app_mobile/app_utils/ServerDestination.java
+++
@@ -1,9 +0,0 @@
-package com.group06.music_app_mobile.app_utils;
-
-public class ServerDestination {
-
-    public static final String SERVER_HOST = "192.168.1.9";
-    public static final String SERVER_PORT = "8080";
-
-    private ServerDestination() {}
-}
